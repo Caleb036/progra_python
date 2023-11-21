e05c2cb4617362784cdbd4aaa59a8c41c6113092
Caleb036 <151097209+Caleb036@users.noreply.github.com>
2023-11-20 20:32:45 -0600

    Update evidencia2.py

--- evidencia2.py
+++ evidencia2.py
@@ -10,3 +10,14 @@ class Persona:
     def cumplir_anios(self):
         self.edad += 1
         print(f"¡Feliz cumpleaños, {self.nombre}! Ahora tienes {self.edad} años.")
+
+persona1 = Persona("Juan", 25)
+
+print(persona1.nombre)  
+print(persona1.edad)    
+
+print(persona1.info)    
+
+persona1.cumplir_anios()  
+
+print(persona1.edad)    
